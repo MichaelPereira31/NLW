b152a7ff4b83127bd85832785f6178efa7501535
Michael <michaelpereira408@gmail.com>
2022-04-30 16:09:06 -0300

    feat: add uuid and created_at in categories

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,15 +1,20 @@
-import { Router } from "express"
-
+import { Router } from 'express'
+import {v4 as uuid} from 'uuid'
 const categoriesRoutes = Router()
 
 const categories = [];
-categoriesRoutes.post('/categories', (request,response) =>{
+
+categoriesRoutes.post('/', (request,response) =>{
     const {name, description} = request.body
 
-    categories.push({
-        name, description
-    });
-
+    const category = {
+        id: uuid(),
+        name, 
+        description,
+        created_at: new Date(),
+    }
+    categories.push(category);
+    console.log(categories)
     return response.status(201).send()
 
 })
