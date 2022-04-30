3605efa47302612ea8588db1e4f699ee8c5c518f
Michael <michaelpereira408@gmail.com>
2022-04-30 16:21:21 -0300

    refactor: entering type for category

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,21 +1,19 @@
 import { Router } from 'express'
 import {v4 as uuid} from 'uuid'
+import { Category } from '../model/Category';
 const categoriesRoutes = Router()
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post('/', (request,response) =>{
     const {name, description} = request.body
 
-    const category = {
-        id: uuid(),
-        name, 
-        description,
-        created_at: new Date(),
-    }
+    const category = new Category()
+
+    Object.assign(category, {name, description,created_at: new Date()})
+
     categories.push(category);
-    console.log(categories)
-    return response.status(201).send()
+    return response.status(201).json({category})
 
 })
 
